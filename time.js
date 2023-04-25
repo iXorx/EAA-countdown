245125871e923fe6a791468be7784a1c93131870
Jordi Turull <jordi.frontend@gmail.com>
2023-04-26 00:36:10 +0200

    feat: WPO

---
+++ time.js
@@ -0,0 +1,33 @@
+const eadDueDate = new Date("Jun 28, 2025 00:00:00").getTime();
+const now = new Date().getTime();
+
+const distance = eadDueDate - now;
+const years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+document.getElementById("y-1").innerHTML = 0;
+const yearsSecondDigit = years % 10;
+document.getElementById("y-2").innerHTML = yearsSecondDigit;
+
+const months = Math.floor(
+  (distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30)
+);
+const monthsFirstDigit = Math.floor(months / 10);
+document.getElementById("m-1").innerHTML =
+  monthsFirstDigit > 0 ? monthsFirstDigit : 0;
+const monthsSecondDigit = months % 10;
+document.getElementById("m-2").innerHTML = monthsSecondDigit;
+
+let days = Math.floor(
+  (distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24)
+);
+while (days >= 30) {
+  days -= 30;
+}
+const daysFirstDigit = Math.floor(days / 10);
+document.getElementById("d-1").innerHTML =
+  daysFirstDigit > 0 ? daysFirstDigit : 0;
+const daysSecondDigit = days % 10;
+document.getElementById("d-2").innerHTML = daysSecondDigit;
+
+// Display the result in the element with id="demo"
+document.getElementById("countdown").innerHTML =
+  years + " years " + months + " months " + days + " days ";
